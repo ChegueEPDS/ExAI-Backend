49244e4360f2fabbe2accc453fcc30294a2b3d36
ChegueEPDS <kovacs@epds.hu>
2025-06-05 09:10:13 +0200

    instruction véd

--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const authMiddleware = require('../middlewares/authMiddleware');
 const openAIController = require('../controllers/openaiController');
 
-router.get('/instructions', authMiddleware(['Admin']), openAIController.getAssistantInstructions);
+router.get('/instructions', authMiddleware(), openAIController.getAssistantInstructions);
 
 module.exports = router;
